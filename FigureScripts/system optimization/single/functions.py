56f7a964f982021ef6e8794a6abbc423e0536ab7
wywclmqf <wywclmqf@gmail.com>
2022-04-25 05:45:17 +0200

    new baseline

---
+++ FigureScripts/system optimization/single/functions.py
@@ -0,0 +1,77 @@
+import os
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.express as px
+
+pio.kaleido.scope.mathjax = None
+
+windowType = ['Median', 'Quantile', 'Average', 'Sum', 'Count', 'Max', 'Min']
+widthSin = 0.25
+widthAll=[widthSin,widthSin,widthSin,widthSin,widthSin]
+
+fig = go.Figure()
+
+fig.add_trace(go.Bar(name="DesisCen", x=windowType, y=[1626861.266, 1622474.069, 3065244.36, 3056406.42, 3104004.67, 3192249.027, 3210081.02], legendrank=1, width=widthAll
+                     , marker_line_color='rgb(99,110,250)', marker_pattern_shape="."))
+fig.add_trace(go.Bar(name="DesisSW", x=windowType, y=[7387008.61, 7381403.005, 30635560.52, 30651978.27, 30811652.63, 34126088.52, 34653452.05], legendrank=2, width=widthAll
+                     , marker_line_color='rgb(255,161,90)', marker_pattern_shape="-"))
+fig.add_trace(go.Bar(name="Desis", x=windowType, y=[7401292, 7365279.748, 30545075.4, 30447031.23, 30935064.94, 34251061.44, 34515041.78], legendrank=3, width=widthAll
+                     , marker_line_color='rgb(171,99,250)', marker_pattern_shape="\\"))
+# fig.add_trace(go.Bar(name="DesisSw", x=[" "], y=[30545075.4], legendrank=4, width=[0.18]
+#                      , marker_line_color='rgb(255,161,90)', marker_pattern_shape="-"))
+# fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
+
+7401292, 7365279.748, 30545075.4, 30447031.23, 30935064.94, 34251061.44, 34515041.78
+7387008.61, 7381403.005, 30635560.52, 30651978.27, 30811652.63, 34126088.52, 34653452.05
+1626861.266, 1622474.069, 3065244.36, 3056406.42, 3104004.67, 3192249.027, 3210081.02
+
+
+#legend
+fig.update_layout(
+    legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01,
+        # bordercolor="Black",
+        # borderwidth=2,
+        # bgcolor="white",
+        font=dict(
+            size=10,
+            color="black"
+        ),
+    ),
+    yaxis=dict(
+        title_text="events/sec",
+        titlefont=dict(size=15),
+        exponentformat="e",
+        ticktext=["0", "2M", "5M", "10M", "20M", "30M"],
+        tickvals=[0, 2000000, 5000000, 10000000, 20000000, 30000000],
+        tickmode="array",
+        range=[0, 35000000],
+    )
+)
+
+# size & color
+fig.update_layout(
+    autosize=False,
+    width=500,
+    height=500,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+# fig = px.bar(x=["a","b","c"], y=[1,3,2], color=["red", "goldenrod", "#00D"], color_discrete_map="identity")
+fig.update_layout(barmode='group', bargap=0.2, bargroupgap=0.0)
+
+# fig.update_yaxes(automargin=True)
+# fig.update_yaxes(ticks="outside", tickwidth=1, tickcolor='black', ticklen=5)
+fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+
+
+fig.show()
+if not os.path.exists("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2"):
+    os.mkdir("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2")
+# fig.write_image("images/fig1.svg")
+pio.write_image(fig, "E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2\singleFunctions.pdf")
+pio.write_image(fig, "E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2\singleFunctions.svg")
---
+++ FigureScripts/system optimization/single/windowTypes.py
@@ -0,0 +1,74 @@
+import os
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.express as px
+
+pio.kaleido.scope.mathjax = None
+
+windowType = ['Tumbling', 'Sliding', 'Punctuation', 'Session', 'Countbased']
+widthSin = 0.25
+widthAll=[widthSin,widthSin,widthSin,widthSin,widthSin]
+
+fig = go.Figure()
+
+fig.add_trace(go.Bar(name="DesisCen", x=windowType, y=[3065244.36, 1463248.92, 2062724.5, 2041280.4, 1937204.07], legendrank=1, width=widthAll
+                     , marker_line_color='rgb(99,110,250)', marker_pattern_shape="."))
+fig.add_trace(go.Bar(name="DesisSW", x=windowType, y=[30635560.52, 30094194.45, 31055213.25, 31169257.83, 16001392.57], legendrank=2, width=widthAll
+                     , marker_line_color='rgb(255,161,90)', marker_pattern_shape="-"))
+fig.add_trace(go.Bar(name="Desis", x=windowType, y=[30545075.4, 29811997.74, 30980630.5, 31137398.54, 16012225.69], legendrank=3, width=widthAll
+                     , marker_line_color='rgb(171,99,250)', marker_pattern_shape="\\"))
+# fig.add_trace(go.Bar(name="DesisSw", x=[" "], y=[30545075.4], legendrank=4, width=[0.18]
+#                      , marker_line_color='rgb(255,161,90)', marker_pattern_shape="-"))
+# fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
+
+
+
+#legend
+fig.update_layout(
+    legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01,
+        # bordercolor="Black",
+        # borderwidth=2,
+        # bgcolor="white",
+        font=dict(
+            size=10,
+            color="black"
+        ),
+    ),
+    yaxis=dict(
+        title_text="events/sec",
+        titlefont=dict(size=15),
+        exponentformat="e",
+        ticktext=["0", "2M", "5M", "10M", "20M", "30M"],
+        tickvals=[0, 2000000, 5000000, 10000000, 20000000, 30000000],
+        tickmode="array",
+        range=[0, 35000000],
+    )
+)
+
+# size & color
+fig.update_layout(
+    autosize=False,
+    width=500,
+    height=500,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+# fig = px.bar(x=["a","b","c"], y=[1,3,2], color=["red", "goldenrod", "#00D"], color_discrete_map="identity")
+fig.update_layout(barmode='group', bargap=0.2, bargroupgap=0.0)
+
+# fig.update_yaxes(automargin=True)
+# fig.update_yaxes(ticks="outside", tickwidth=1, tickcolor='black', ticklen=5)
+fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+
+
+fig.show()
+if not os.path.exists("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2"):
+    os.mkdir("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2")
+# fig.write_image("images/fig1.svg")
+pio.write_image(fig, "E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2\singleWindow.pdf")
+pio.write_image(fig, "E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2\singleWindow.svg")
