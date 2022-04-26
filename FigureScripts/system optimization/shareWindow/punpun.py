6445610b6450f9be0002a8b38eb8bdd9325bd3b9
wywclqmf <wywclmqf@gmail.com>
2022-04-26 21:09:04 +0200

    result

---
+++ FigureScripts/system optimization/shareWindow/punpun.py
@@ -0,0 +1,77 @@
+import os
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.express as px
+
+pio.kaleido.scope.mathjax = None
+
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(name="DesisCen", x=[2, 10, 50, 100, 500, 1000], mode='lines+markers'
+                         , y=[940397.351, 812715.2795, 580393.6364, 458268.6379, 230187.9963, 179682.7369]
+                         , line=dict(color='rgb(99,110,250)', width=2), marker=dict(size=5, symbol='circle')))
+fig.add_trace(go.Scatter(name="DesisSW", x=[2, 10, 50, 100, 500, 1000], mode='lines+markers'
+                         , y=[30105488.54, 29247925.01, 28903914.93, 28867106.34, 23352343.31, 20380503.55]
+                         , line=dict(color='rgb(255,161,90)', width=2), marker=dict(size=5, symbol='triangle-up')))
+fig.add_trace(go.Scatter(name="Desis", x=[2, 10, 50, 100, 500, 1000], mode='lines+markers'
+                         , y=[30302154.37, 29279503.11, 29755302.67, 29368602.41, 28875997.16, 29934606.84]
+                         , line=dict(color='rgb(171,99,250)', width=2), marker=dict(size=5, symbol='cross')))
+
+# fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6,
+# marker=dict(size=10, symbol='triangle-up'))))
+
+#legend
+fig.update_layout(
+    legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01,
+        # bordercolor="Black",
+        # borderwidth=2,
+        # bgcolor="white",
+        font=dict(
+            size=10,
+            color="black"
+        ),
+    ),
+    yaxis=dict(
+        title_text="events/sec",
+        titlefont=dict(size=15),
+        ticktext=["0", "2M", "5M", "10M", "20M", "30M"],
+        tickvals=[0, 2000000, 5000000, 10000000, 20000000, 30000000],
+        tickmode="array",
+        range=[0, 32000000],
+    ),
+    xaxis=dict(
+        title_text="queries",
+        titlefont=dict(size=15),
+        ticktext=["1", "10", '100', "1000"],
+        tickvals=[1, 10, 100, 1000],
+        range=[0,3],
+        type="log"
+    )
+)
+
+# size & color
+fig.update_layout(
+    autosize=False,
+    width=500,
+    height=500,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+
+# fig.update_yaxes(automargin=True)
+# fig.update_yaxes(ticks="outside", tickwidth=1, tickcolor='black', ticklen=5)
+fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+
+fig.show()
+if not os.path.exists("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2"):
+    os.mkdir("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2")
+
+# fig.write_image("images/fig1.svg")
+pio.write_image(fig, "E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2\punpun.pdf")
+pio.write_image(fig, "E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2\punpun.svg")
---
+++ FigureScripts/system optimization/sharefunction/avgsum.py
@@ -0,0 +1,79 @@
+import os
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.express as px
+
+pio.kaleido.scope.mathjax = None
+
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(name="DesisCen", x=[2, 10, 50, 100, 500, 1000], mode='lines+markers'
+                         , y=[2673205.93, 1006913.8, 662475.64, 223458.038, 144271.15, 139632.158]
+                         , line=dict(color='rgb(99,110,250)', width=2), marker=dict(size=5, symbol='circle')))
+fig.add_trace(go.Scatter(name="DesisSW", x=[2, 10, 50, 100, 500, 1000], mode='lines+markers'
+                         , y=[30060452.29, 27474163.52, 26134213.48, 24472053.49, 21037626, 18811170]
+                         , line=dict(color='rgb(255,161,90)', width=2), marker=dict(size=5, symbol='triangle-up')))
+fig.add_trace(go.Scatter(name="Desis", x=[2, 10, 50, 100, 500, 1000], mode='lines+markers'
+                         , y=[30445075.4, 30270030.66, 29985328.87, 29893363.04, 29872764.4, 29864216.82]
+                         , line=dict(color='rgb(171,99,250)', width=2), marker=dict(size=5, symbol='cross')))
+
+# fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6,
+# marker=dict(size=10, symbol='triangle-up'))))
+
+
+
+#legend
+fig.update_layout(
+    legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01,
+        # bordercolor="Black",
+        # borderwidth=2,
+        # bgcolor="white",
+        font=dict(
+            size=10,
+            color="black"
+        ),
+    ),
+    yaxis=dict(
+        title_text="events/sec",
+        titlefont=dict(size=15),
+        ticktext=["0", "2M", "5M", "10M", "20M", "30M"],
+        tickvals=[0, 2000000, 5000000, 10000000, 20000000, 30000000],
+        tickmode="array",
+        range=[0, 32000000],
+    ),
+    xaxis=dict(
+        title_text="queries",
+        titlefont=dict(size=15),
+        ticktext=["1", "10", '100', "1000"],
+        tickvals=[1, 10, 100, 1000],
+        range=[0,3],
+        type="log"
+    )
+)
+
+# size & color
+fig.update_layout(
+    autosize=False,
+    width=500,
+    height=500,
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)'
+)
+
+# fig.update_yaxes(automargin=True)
+# fig.update_yaxes(ticks="outside", tickwidth=1, tickcolor='black', ticklen=5)
+fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+
+fig.show()
+if not os.path.exists("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2"):
+    os.mkdir("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2")
+
+# fig.write_image("images/fig1.svg")
+pio.write_image(fig, "E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2\/avgsum.pdf")
+pio.write_image(fig, "E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s2\/avgsum.svg")
