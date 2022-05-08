8919aa4b259b483da49258048b8915123f65b7c5
wywclmqf <wywclmqf@gmail.com>
2022-05-08 22:41:01 +0200

    exp

--- FigureScripts/real data/real data.py
+++ FigureScripts/real data/real data.py
@@ -5,25 +5,26 @@ import plotly.express as px
 
 pio.kaleido.scope.mathjax = None
 
-xaxis = [10, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+xaxis = [10, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 50000, 100000, 500000, 1000000]
 
 fig = go.Figure()
 
 fig.add_trace(go.Scatter(name="DesisCen", x=xaxis, mode='lines+markers'
-                         , y=[80904.27, 77070.0052, 100287.23, 85969.64, 45931.5399, 5836.943, 569.05558, 3317.3286, 1436.53516]
+                         , y=[80904.27, 77070.0052, 100287.23, 85969.64, 45931.5399, 5836.943, 569.05558, 3317.3286, 3239.632366, 3187.9419, 3157.711, 3118.2859, 3089.3565]
                          , line=dict(color='rgb(99,110,250)', width=2), marker=dict(size=5, symbol='circle')))
 fig.add_trace(go.Scatter(name="DesisIC", x=xaxis, mode='lines+markers'
-                         , y=[3769428.71, 614002.09, 276933.59, 113125.26, 50027.37, 25677.109, 8050.648, 4493.958, 3556.81]
+                         , y=[3769428.71, 614002.09, 276933.59, 113125.26, 50027.37, 25677.109, 8050.648, 4493.958, 3556.81, 3232.43, 3217.23, 3189.73, 3060.95]
                          , line=dict(color='rgb(239,85,59)', width=2), marker=dict(size=5, symbol='square')))
 fig.add_trace(go.Scatter(name="DesisSW", x=xaxis, mode='lines+markers'
-                         , y=[7087908.72, 7115832.48, 7088310.52, 7082815.01, 6926315.17, 7110177.066, 6842615.58, 6916566.4, 7062998.53]
+                         , y=[7087908.72, 7115832.48, 7088310.52, 7082815.01, 6926315.17, 7110177.066, 6842615.58, 6916566.4, 7062998.53, 6664863.798, 5831647.58, 5085647.68, 4604147.195]
                          , line=dict(color='rgb(255,161,90)', width=2), marker=dict(size=5, symbol='triangle-up')))
 fig.add_trace(go.Scatter(name="Desis", x=xaxis, mode='lines+markers'
-                         , y=[25037257.88, 24390015.56, 24248868.78, 24602546.02, 23256023.72, 23686170.7, 23284746.21, 23700784.04, 23483457.72]
+                         , y=[25037257.88, 24390015.56, 24248868.78, 24602546.02, 23256023.72, 23686170.7, 23284746.21, 23700784.04, 23483457.72, 24658974.78, 21502353.92, 19009147.37, 17712309.55]
                          , line=dict(color='rgb(171,99,250)', width=2), marker=dict(size=5, symbol='cross')))
 
 # fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6,
 # marker=dict(size=10, symbol='triangle-up'))))
+
 
 #legend
 fig.update_layout(
@@ -51,12 +52,13 @@ fig.update_layout(
     xaxis=dict(
         title_text="queries",
         titlefont=dict(size=15),
-        ticktext=["10", '100', "1000", "10000"],
-        tickvals=[10, 100, 1000, 10000],
-        range=[1,4],
+        ticktext=["10", '100', "1000", "10000", "100000", "1000000"],
+        tickvals=[10, 100, 1000, 10000, 100000, 1000000],
+        range=[1,6],
         type="log"
     )
 )
+
 
 # size & color
 fig.update_layout(
--- FigureScripts/system optimization/single/functions.py
+++ FigureScripts/system optimization/single/functions.py
@@ -24,6 +24,7 @@ fig.add_trace(go.Bar(name="Desis", x=windowType, y=[7401292, 7345260.74, 3242928
 # fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
 
 
+
 #legend
 fig.update_layout(
     # legend=dict(
