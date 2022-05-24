a442ede50d021c3dd61393ff040c31b2e5abea70
wywclqmf <wywclmqf@gmail.com>
2022-05-24 22:47:22 +0200

    result

--- FigureScripts/system performance/scalability/average.py
+++ FigureScripts/system performance/scalability/average.py
@@ -10,62 +10,81 @@ fig = go.Figure()
 
 fig.add_trace(go.Scatter(name="Central", x=[1, 2, 3, 4, 5, 6, 7, 8], mode='lines+markers'
                          , y=[3268487.831, 3270697.149, 3291610.9, 3295282.32, 3308962.22, 3311921.619, 3298082.798, 3246734.899]
-                         , line=dict(color='rgb(99,110,250)', width=2), marker=dict(size=5, symbol='circle')))
+                         , line=dict(color='rgb(153, 0, 50)', width=5), marker=dict(size=20, symbol='circle')))
 fig.add_trace(go.Scatter(name="Disco", x=[1, 2, 3, 4, 5, 6, 7, 8], mode='lines+markers'
                          , y=[2181646, 4248293, 6214096.62, 8234068.64, 10462675.8, 13089876, 14869027.39, 16262696.7]
-                         , line=dict(color='rgb(255,161,90)', width=2), marker=dict(size=5, symbol='square')))
+                         , line=dict(color='rgb(49, 0, 125)', width=5), marker=dict(size=20, symbol='square')))
 fig.add_trace(go.Scatter(name="Desis", x=[1, 2, 3, 4, 5, 6, 7, 8], mode='lines+markers'
                          , y=[32801883.73, 62691950.2, 94548761.94, 128747306.2, 156350305.4, 186176314.4, 222421423.5, 240059496.1]
-                         , line=dict(color='rgb(171,99,250)', width=2), marker=dict(size=5, symbol='cross')))
+                         , line=dict(color='rgb(60,60,60)', width=5), marker=dict(size=20, symbol='cross')))
 
 # fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6,
 # marker=dict(size=10, symbol='triangle-up'))))
 
 #legend
 fig.update_layout(
-    # legend=dict(
-    #     yanchor="top",
-    #     y=0.99,
-    #     xanchor="left",
-    #     x=0.01,
-    #     # bordercolor="Black",
-    #     # borderwidth=2,
-    #     # bgcolor="white",
-    #     font=dict(
-    #         size=10,
-    #         color="black"
-    #     ),
-    # ),
+    legend=dict(
+        orientation="h",
+        yanchor="top",
+        y=1.2,
+        xanchor="left",
+        x=0.1,
+        # bordercolor="Black",
+        # borderwidth=2,
+        # bgcolor="white",
+        font=dict(
+            size=30,
+            color="black"
+        ),
+    ),
     yaxis=dict(
         title_text="events/sec",
-        titlefont=dict(size=15),
-        # ticktext=["0", "3M", "5M", "10M", "100M", "300M"],
-        # tickvals=[0, 3000000, 5000000, 10000000, 100000000, 300000/00],
+        titlefont=dict(size=35),
+        exponentformat="e",
+        ticktext=["0", "5M", "10M", "15M", "20M", "25M"],
+        tickvals=[0, 50000000, 100000000, 150000000, 200000000, 250000000],
         tickmode="array",
+        range=[0, 260000000],
+        tickfont=dict(size=35),
+        # ticks="inside",
+        # ticklen=20,
+        # tickwidth =5,
     ),
+
     xaxis=dict(
         title_text="local nodes",
-        titlefont=dict(size=15),
+        titlefont=dict(size=35),
         ticktext=["1", "2", "3", "4", "5", "6", "7", "8"],
         tickvals=[1, 2, 3, 4, 5, 6, 7, 8],
         range=[1,8],
         tickmode="array",
+        ticks="inside",
+        ticklen=20,
+        tickwidth=5,
+        tickfont=dict(size=35),
     )
 )
 
 # size & color
 fig.update_layout(
     autosize=False,
-    width=500,
-    height=500,
+    width=660,
+    height=440,
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)'
+    plot_bgcolor='rgba(0,0,0,0)',
+    margin=dict(
+        l=5,
+        r=5,
+        b=5,
+        t=5,
+        pad=0
+    ),
 )
-
-# fig.update_yaxes(automargin=True)
-# fig.update_yaxes(ticks="outside", tickwidth=1, tickcolor='black', ticklen=5)
-fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
-fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+fig.update_xaxes(showline=True, linewidth=3, linecolor='black'#, mirror=True
+                 )
+fig.update_yaxes(showline=True, linewidth=3, linecolor='black'#, mirror=True
+                 )
+fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='rgb(120,120,120)')
 
 fig.show()
 if not os.path.exists("E:\my paper\DesisPaper\Desis-Optimizing-Decentralized-Window-Aggregation\experiment\s1"):
