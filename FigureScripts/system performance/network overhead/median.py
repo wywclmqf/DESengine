3e69c5a431b19df1237385d1c3ff68f0aa73239a
wywclqmf <wywclmqf@gmail.com>
2022-05-24 22:47:22 +0200

    result

--- FigureScripts/system performance/network overhead/median.py
+++ FigureScripts/system performance/network overhead/median.py
@@ -8,58 +8,80 @@ pio.kaleido.scope.mathjax = None
 
 fig = go.Figure()
 
-fig.add_trace(go.Bar(name="Central", x=[" "], y=[2.982], legendrank=1, width=[0.25]
-                     , marker_line_color='rgb(99,110,250)', marker_pattern_shape="."))
-fig.add_trace(go.Bar(name="Disco", x=[" "], y=[3.3528], legendrank=2, width=[0.25]
-                     , marker_line_color='rgb(239,85,59)', marker_pattern_shape="/"))
-fig.add_trace(go.Bar(name="Desis", x=[" "], y=[2.981], legendrank=3, width=[0.25]
-                     , marker_line_color='rgb(171,99,250)', marker_pattern_shape="\\"))
-# fig.add_trace(go.Bar(name="DesisSw", x=[" "], y=[30545075.4], legendrank=4, width=[0.18]
-#                      , marker_line_color='rgb(255,161,90)', marker_pattern_shape="-"))
-# fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)', marker_line_width=1.5, opacity=0.6)
+fig.add_trace(go.Bar(name="Central", x=["Central"], y=[2.982], legendrank=1, width=[0.8]
+                     , text="<b>2.98GB<b>", textposition='outside', textfont = dict(size = 25)
+                     , marker_color='rgb(60,60,60)'))
+fig.add_trace(go.Bar(name="Disco", x=["Disco"], y=[3.3528], width=[0.8]
+                     , text="<b>3.35GB<b>", textposition='outside', textfont = dict(size = 25)
+                     , marker_color='rgb(60,60,60)'))
+fig.add_trace(go.Bar(name="Desis", x=["Desis"], y=[2.981], width=[0.8]
+                     , text="<b>2.98GB<b>", textposition='outside', textfont = dict(size = 25)
+                     , marker_color='rgb(60,60,60)'))
 
 
 
 #legend
+fig.update_layout(showlegend=False)
 fig.update_layout(
-    legend=dict(
-        yanchor="top",
-        y=0.99,
-        xanchor="left",
-        x=0.01,
-        # bordercolor="Black",
-        # borderwidth=2,
-        # bgcolor="white",
-        font=dict(
-            size=10,
-            color="black"
-        ),
-    ),
+    # legend=dict(
+    #     yanchor="top",
+    #     y=0.99,
+    #     xanchor="left",
+    #     x=0.01,
+    #     # bordercolor="Black",
+    #     # borderwidth=2,
+    #     # bgcolor="white",
+    #     font=dict(
+    #         size=20,
+    #         color="black"
+    #     ),
+    # ),
     yaxis=dict(
         title_text="bytes sent in GB",
-        titlefont=dict(size=15),
-        ticktext=["0", "1GB", "2GB", "3GB"],
-        tickvals=[0, 1, 2, 3],
+        titlefont=dict(size=35),
+        ticktext=["0", "1GB", "2GB", "3GB", "4GB"],
+        tickvals=[0, 1, 2, 3, 4],
         tickmode="array",
-        range=[0, 4],
-    )
+        range=[0, 4.1],
+        ticks="inside",
+        ticklen=20,
+        tickwidth=5,
+    ),
+    xaxis = dict(
+        # title_text="local nodes",
+        # titlefont=dict(size=15),
+        ticktext=["Central", "Disco", "Desis"],
+        # tickvals=[1, 2, 3, 4],
+        tickmode="array",
+        # range=[0, 6],
+    ),
 )
 
 # size & color
 fig.update_layout(
     autosize=False,
-    width=350,
-    height=500,
+    width=660,
+    height=440,
     paper_bgcolor='rgba(0,0,0,0)',
-    plot_bgcolor='rgba(0,0,0,0)'
+    plot_bgcolor='rgba(0,0,0,0)',
+    margin=dict(
+        l=5,
+        r=5,
+        b=5,
+        t=5,
+        pad=0
+    ),
 )
 # fig = px.bar(x=["a","b","c"], y=[1,3,2], color=["red", "goldenrod", "#00D"], color_discrete_map="identity")
-fig.update_layout(barmode='group', bargap=0.2, bargroupgap=0.0)
+fig.update_layout(barmode='group', bargap=0.4)
 
 # fig.update_yaxes(automargin=True)
 # fig.update_yaxes(ticks="outside", tickwidth=1, tickcolor='black', ticklen=5)
-fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
-fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+fig.update_xaxes(showline=True, linewidth=3, linecolor='black'#, mirror=True
+                 , tickfont=dict(size=35))
+fig.update_yaxes(showline=True, linewidth=3, linecolor='black'#, mirror=True
+                 , tickfont=dict(size=35))
+
 
 
 fig.show()
